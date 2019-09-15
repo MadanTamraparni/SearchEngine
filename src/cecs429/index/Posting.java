21f53e28db3c04a62936b11c5bfeef66c26a2ecc
Charles Kwak <charles971026@gmail.com>
2019-09-14 17:29:17 -0700

    Add files via upload

--- src/cecs429/index/Posting.java
+++ src/cecs429/index/Posting.java
@@ -1,16 +1,28 @@
 package cecs429.index;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * A Posting encapulates a document ID associated with a search query component.
+ * A Posting encapsulates a document ID associated with a search query component.
  */
 public class Posting {
 	private int mDocumentId;
+	private List<Integer> mPositions;
 	
 	public Posting(int documentId) {
 		mDocumentId = documentId;
+		mPositions = new ArrayList<Integer>();
 	}
 	
+	public void addPosition(int position){
+		mPositions.add(position);
+	}
 	public int getDocumentId() {
 		return mDocumentId;
+	}
+	
+	public List<Integer> getPositions(){
+		return mPositions;
 	}
 }
