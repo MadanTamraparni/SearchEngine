88404700d8ac96cae4b358af43b1c500e3e0e1b6
Madan Tamraparni <madan.madan89@gmail.com>
2019-09-17 10:40:49 -0700

    Added Enhance process token

--- src/cecs429/text/BasicTokenProcessor.java
+++ src/cecs429/text/BasicTokenProcessor.java
@@ -1,12 +1,46 @@
 package cecs429.text;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 /**
  * A BasicTokenProcessor creates terms from tokens by removing all non-alphanumeric characters from the token, and
  * converting it to all lowercase.
  */
 public class BasicTokenProcessor implements TokenProcessor {
+	public final String STR_HYPHEN = "-";
+	private final PorterStemmer m_Stemmer = new PorterStemmer();
 	@Override
 	public String processToken(String token) {
+		token = m_Stemmer.GetStemmedToken(token);
 		return token.replaceAll("\\W", "").toLowerCase();
 	}
+	
+	@Override
+	public List<String> enhancedProcessToken(String token) {
+		// TODO Auto-generated method stub
+		token = token.replaceAll("[^A-Za-z]", "");
+		token = token.replaceAll("\\W", "").toLowerCase();
+		List<String> tokenList = processHypenToken(token);
+		return tokenList;
+	}
+	
+	private List<String> processHypenToken(String token)
+	{
+		List<String> listHyphenToken = new ArrayList<String>(); 
+		if(token.contains(STR_HYPHEN))
+		{
+			StringTokenizer tokenizer = new StringTokenizer(token,"-");
+			while(tokenizer.hasMoreTokens())
+			{
+				listHyphenToken.add(m_Stemmer.GetStemmedToken(tokenizer.nextToken()));
+			}
+		}
+		else
+			listHyphenToken.add(m_Stemmer.GetStemmedToken(token));
+		
+		return listHyphenToken;
+	}
+		
 }
