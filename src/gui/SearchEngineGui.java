1e7157f199bea5843e243e17a6cd5ff618e3aae2
Charles Kwak <charles971026@gmail.com>
2019-10-05 13:25:43 -0700

    Commented

--- src/gui/SearchEngineGui.java
+++ src/gui/SearchEngineGui.java
@@ -1,6 +1,5 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -14,7 +13,6 @@ import cecs429.index.Index;
 import cecs429.index.PositionalInvertedIndex;
 import cecs429.index.Posting;
 import cecs429.query.BooleanQueryParser;
-import cecs429.query.NearLiteral;
 import cecs429.query.QueryComponent;
 import cecs429.text.BasicTokenProcessor;
 import cecs429.text.EnglishTokenStream;
@@ -22,38 +20,34 @@ import cecs429.text.PorterStemmer;
 import cecs429.text.TokenProcessor;
 
 import javax.swing.JTextField;
-import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.List;
 import java.awt.event.ActionEvent;
+import javax.swing.DropMode;
 
 public class SearchEngineGui extends JFrame {
 	
-	Index index;
-	BooleanQueryParser queryParser = new BooleanQueryParser();
-	DocumentCorpus corpus;
+	private Index mIndex;
+	private BooleanQueryParser mQueryParser = new BooleanQueryParser();
+	private DocumentCorpus mCorpus;
 	
-	private JPanel contentPane;
-	private JTextField txtDirectory;
-	private JTextField txtSearch;
+	private JPanel mContentPane;
+	private JTextField mTextDirectory;
+	private JTextField mTextSearch;
 	
 	public static final String STEM_STR = "stem";
 	public static final String QUIT_STR = "q";
 	public static final String INDEX_STR = "index";
 	public static final String VOCAB_STR = "vocab";
-	public static final char NEAR_STR = '[';
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		
-		
 		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -74,46 +68,49 @@ public class SearchEngineGui extends JFrame {
 	
 	public SearchEngineGui() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 531, 392);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		setBounds(100, 100, 561, 415);
+		mContentPane = new JPanel();
+		mContentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(mContentPane);
+		mContentPane.setLayout(null);
 		
-		txtDirectory = new JTextField();
-		txtDirectory.setBounds(6, 6, 459, 20);
-		txtDirectory.setText("Enter document directory");
-		contentPane.add(txtDirectory);
-		txtDirectory.setColumns(10);
+		//text field for directory
+		mTextDirectory = new JTextField();
+		mTextDirectory.setBounds(6, 5, 459, 20);
+		mContentPane.add(mTextDirectory);
+		mTextDirectory.setColumns(10);
 		
+		//button for indexing
+		JButton directoryButton = new JButton("Index");
+		directoryButton.setBounds(468, 6, 87, 20);
+		mContentPane.add(directoryButton);
 		
-		JButton directoryButton = new JButton("Return");
-		directoryButton.setBounds(468, 6, 57, 20);
-		contentPane.add(directoryButton);
+		//text field for search
+		mTextSearch = new JTextField();
+		mTextSearch.setBounds(6, 30, 459, 20);
+		mContentPane.add(mTextSearch);
+		mTextSearch.setColumns(10);
 		
-		txtSearch = new JTextField();
-		txtSearch.setBounds(6, 30, 459, 20);
-		txtSearch.setText("Search ");
-		contentPane.add(txtSearch);
-		txtSearch.setColumns(10);
+		//button for searching
+		JButton searchButton = new JButton("Search");
+		searchButton.setBounds(468, 30, 87, 20);
+		mContentPane.add(searchButton);
 		
-		JButton searchButton = new JButton("Return");
-		searchButton.setBounds(468, 30, 57, 20);
-		contentPane.add(searchButton);
-		
+		//scroll for text area
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(6, 55, 519, 309);
-		contentPane.add(scrollPane);
+		scrollPane.setBounds(6, 55, 549, 332);
+		mContentPane.add(scrollPane);
 		
+		//text area for output
 		JTextArea textArea = new JTextArea();
 		scrollPane.setViewportView(textArea);
 		
-		//Return button for getting the corpus and indexing terms
+		//action listener for index button: getting the corpus and indexing terms
 		directoryButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String path;
 				while(true){
-		            path = txtDirectory.getText();
+		            path = mTextDirectory.getText();
 		            File testDir = new File(path);
 		            if(testDir.isDirectory()){
 		            	textArea.append("Directory Existed. Procceed to indexing..."); 
@@ -123,8 +120,8 @@ public class SearchEngineGui extends JFrame {
 		        }
 				
 				long timeStart = System.currentTimeMillis();
-		        corpus = DirectoryCorpus.loadJsonDirectory(new File(path).toPath(),".json");
-		        index = indexCorpus(corpus);
+		        mCorpus = DirectoryCorpus.loadJsonDirectory(new File(path).toPath(),".json");
+		        mIndex = indexCorpus(mCorpus);
 		        long timeEnd = System.currentTimeMillis();
 		        long time = timeEnd - timeStart;
 		        double seconds = time / 1000.0;
@@ -132,16 +129,16 @@ public class SearchEngineGui extends JFrame {
 			}
 		});
 		
-		//Return button for searching terms
+		//action listener for search button: searching terms
 		searchButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String query;
 				TokenProcessor  processor = new BasicTokenProcessor();
 				
-				query = txtSearch.getText();
+				query = mTextSearch.getText();
 	            if(query.equals(QUIT_STR))
 	            {
-	            	textArea.append("q entry for quit.\n");
+	            	System.exit(0);
 	            	return;
 	            }
 	            else if(query.contains(STEM_STR))
@@ -158,8 +155,8 @@ public class SearchEngineGui extends JFrame {
 	                if(testDir.isDirectory()){
 	                	textArea.append("Directory Existed. Procceed to indexing...\n"); 
 	    				long timeStart = System.currentTimeMillis();
-	        			corpus = DirectoryCorpus.loadJsonDirectory(new File(query).toPath(),".json");
-	        			index = indexCorpus(corpus) ;
+	        			mCorpus = DirectoryCorpus.loadJsonDirectory(new File(query).toPath(),".json");
+	        			mIndex = indexCorpus(mCorpus) ;
 	        			long timeEnd = System.currentTimeMillis();
 	        			long time = timeEnd - timeStart;
 	    		        double seconds = time / 1000.0;
@@ -171,38 +168,22 @@ public class SearchEngineGui extends JFrame {
 	            }
 	            else if(query.contains(VOCAB_STR))
 	            {
-	            	List<String> vocabList = index.getVocabulary();
+	            	List<String> vocabList = mIndex.getVocabulary();
 	            	for(int i =0; i < 1000; i++){
 	            		textArea.append(vocabList.get(i) + "\n");
 	            	}
 	            	textArea.append("Size of the Vocabulary = " + vocabList.size() + "\n");
 	            	return;
 	            }
-	            else if(query.charAt(0) == NEAR_STR){
-					String[] subString = query.split(" ");
-					NearLiteral near = new NearLiteral(subString[0].substring(1, subString[0].length()), 
-					subString[1].charAt(subString[1].length() - 1),
-					subString[2].substring(0, subString[2].length()- 1));
-					for (Posting p : near.getPostings(index, processor)) {
-						textArea.append("Document ID " + p.getDocumentId() + "\n");
-						// Below print line only for tracing the index
-						textArea.append("Title: " + corpus.getDocument(p.getDocumentId()).getTitle() + "\n");
-					}
-					return;
-				}
+	            
 	            if(query.length() == 0){
 	            	return;
 	            }
 	            
-	            QueryComponent queryComponent = queryParser.parseQuery(query);
-	            textArea.append("Size = " + queryComponent.getPostings(index, processor).size() + "\n");
-	            for (Posting p : queryComponent.getPostings(index, processor)) {
-	            	int docId = p.getDocumentId();
-	            	//if(docId == 0)
-	            		//docId = 1;
-					//System.out.println("Document ID \"article" + docId + ".json\"");
-					// Below print line only for tracing the index
-					textArea.append("Title: " + corpus.getDocument(p.getDocumentId()).getTitle() + "\n");
+	            QueryComponent queryComponent = mQueryParser.parseQuery(query);
+	            textArea.append("Size = " + queryComponent.getPostings(mIndex, processor).size() + "\n");
+	            for (Posting p : queryComponent.getPostings(mIndex, processor)) {
+					textArea.append("Title: " + mCorpus.getDocument(p.getDocumentId()).getTitle() + "\n");
 	            }
 			}
 		});
@@ -212,17 +193,7 @@ public class SearchEngineGui extends JFrame {
 	
 	
 	private static Index indexCorpus(DocumentCorpus corpus) {
-		//HashSet<String> vocabulary = new HashSet<>();
 		BasicTokenProcessor processor = new BasicTokenProcessor();
-		
-		// First, build the vocabulary hash set.
-		
-		// TODO:
-		// Get all the documents in the corpus by calling GetDocuments().
-		// Iterate through the documents, and:
-		// Tokenize the document's content by constructing an EnglishTokenStream around the document's content.
-		// Iterate through the tokens in the document, processing them using a BasicTokenProcessor,
-		//		and adding them to the HashSet vocabulary.
 		
 		PositionalInvertedIndex index = new PositionalInvertedIndex();
 		Iterable<Document> it = corpus.getDocuments();
@@ -240,17 +211,8 @@ public class SearchEngineGui extends JFrame {
 					currentPosition++;
 					index.addTerm(newToken, docId, currentPosition);
 				}
-
-				// ==============================Testing section===========================
-				// Below is old code that not using our enhancedProcessToken
-				// it serve purpose of testing Near operation
-				// currentPosition++;
-				// index.addTerm(processor.processToken(token), docId, currentPosition);
-				// ============================= End testing===============================
 			}
 		}
 		return index;
 	}
-	
-	
 }
