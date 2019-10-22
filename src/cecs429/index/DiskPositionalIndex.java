d7d4dce1a8dc1846edb7a05d3d9814cfd2b648c9
Madan Tamraparni <madan.madan89@gmail.com>
2019-10-22 10:53:59 -0700

    DiskPositionIndex class

---
+++ src/cecs429/index/DiskPositionalIndex.java
@@ -0,0 +1,19 @@
+package cecs429.index;
+
+import java.util.List;
+
+public class DiskPositionalIndex implements Index{
+
+	@Override
+	public List<Posting> getPostings(String term) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getVocabulary() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
