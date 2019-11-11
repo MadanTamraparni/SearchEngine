9dbbb2de85aad5f368f9c20b061d8238aacee562
Charles Kwak <charles971026@gmail.com>
2019-11-10 21:49:47 -0800

    Added token processor to Ranked models

--- src/cecs429/ranked/TfidfModel.java
+++ src/cecs429/ranked/TfidfModel.java
@@ -8,41 +8,48 @@ import java.util.Map;
 
 import cecs429.index.Index;
 import cecs429.index.Posting;
+import cecs429.text.TokenProcessor;
 
 public class TfidfModel implements RankModel {
 	
 	private Index mIndex;
 	private int mCorpusSize;
 	private RandomAccessFile mDocWeightsRaf;
+	private TokenProcessor mProcessor;
 	
-	public TfidfModel(Index index, int corpusSize, RandomAccessFile docWeightsRaf){
+	public TfidfModel(Index index, int corpusSize, RandomAccessFile docWeightsRaf, TokenProcessor processor){
 		mIndex = index;
 		mCorpusSize = corpusSize;
 		mDocWeightsRaf = docWeightsRaf;
+		mProcessor = processor;
 	}
 
 	@Override
 	public HashMap<Integer, Double> rank(String query) throws IOException {
 		String[] queryTerms = query.split(" ");
 		HashMap<Integer,Double> Ad = new HashMap<Integer,Double>();
+		
 		for(String term: queryTerms){
-			
-			List<Posting> termResults = mIndex.getPostings(term);
-			int dft = termResults.size();
-			if(dft == 0) {
-				continue;
-			}
-			double wqt = Math.log(mCorpusSize/(double)termResults.size());
-			
-			for(Posting posting: termResults){
-				int docId = posting.getDocumentId();
-				double wdt = posting.getPositions().size(); //get tftd as wdt
-				if(Ad.containsKey(docId)) {
-					Ad.put(docId, Ad.get(docId)+ wdt*wqt);
-				}else {
-					Ad.put(docId, wdt*wqt);
+			List<String> tokenList = mProcessor.enhancedProcessToken(term);
+			for(String token: tokenList) {
+				List<Posting> tokenResults = mIndex.getPostings(token);
+				int dft = tokenResults.size();
+				if(dft == 0) {
+					continue;
+				}
+				double wqt = Math.log(mCorpusSize/(double)tokenResults.size());
+				
+				for(Posting posting: tokenResults){
+					int docId = posting.getDocumentId();
+					double wdt = posting.getPositions().size(); //get tftd as wdt
+					if(Ad.containsKey(docId)) {
+						Ad.put(docId, Ad.get(docId)+ wdt*wqt);
+					}else {
+						Ad.put(docId, wdt*wqt);
+					}
 				}
 			}
+			
 		}
 		for(Map.Entry<Integer,Double>entry: Ad.entrySet()){
 			mDocWeightsRaf.seek(entry.getKey() * 32);
